ca036de91b5ff5a3d3f22b7c3690363eb9d8df63
Keyproco <oussama.keddar@epitech.eu>
2023-03-03 21:17:52 +0100

    get rid off the skeleton code

--- tests/test_skeleton.py
+++ tests/test_skeleton.py
@@ -1,25 +1,22 @@
 import pytest
 
-from store_api.skeleton import fib, main
+from store_api.skeleton import main
 
 __author__ = "Keyproco"
 __copyright__ = "Keyproco"
 __license__ = "MIT"
 
 
-def test_fib():
-    """API Tests"""
-    assert fib(1) == 1
-    assert fib(2) == 1
-    assert fib(7) == 13
-    with pytest.raises(AssertionError):
-        fib(-10)
+
+
+
+
 
 
 def test_main(capsys):
     """CLI Tests"""
     # capsys is a pytest fixture that allows asserts against stdout/stderr
     # https://docs.pytest.org/en/stable/capture.html
-    main(["7"])
+    main()
     captured = capsys.readouterr()
-    assert "The 7-th Fibonacci number is 13" in captured.out
+    assert "running" in captured.out
