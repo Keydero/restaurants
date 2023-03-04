dced0474e00273397a756a8d0ce87895073e8710
Keyproco <oussama.keddar@epitech.eu>
2023-03-04 10:21:47 +0100

    fast fix for tests, i have to put the correct asserts WIP

--- src/store_api/store.py
+++ src/store_api/store.py
@@ -1,10 +1,6 @@
 from datetime import datetime
 
 
-def get_timestamp():
-    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
-
-
 stores = [
     {
         "name": "WoodyPlanet",
---
+++ tests/test_store.py
@@ -0,0 +1,12 @@
+import pytest
+
+from store_api.store import list
+
+__author__ = "Keyproco"
+__copyright__ = "Keyproco"
+__license__ = "MIT"
+
+
+
+
+
