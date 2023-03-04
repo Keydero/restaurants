966bd90976163f151a70ad109ee45c2207114af1
Keyproco <oussama.keddar@epitech.eu>
2023-03-04 14:55:57 +0100

    add a method to create an item for a specific store

--- src/store_api/store.py
+++ src/store_api/store.py
@@ -31,3 +31,13 @@ def create():
     new_store = {"name": store["name"], "items": []}
     stores.append(new_store)
     return new_store, 201
+
+
+def create_item(name):
+    item = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": item["name"], "price": item["price"]}
+            store["items"].append(new_item)
+            return new_item
+    return {"error": "Store not Found"}, 404
