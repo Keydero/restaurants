2455c3f28a2495265984d72bbed1d74e4aa4009e
Keyproco <oussama.keddar@epitech.eu>
2023-03-12 19:50:33 +0100

    gen new poetry lock file

--- restaurants_api/api/restaurants.py
+++ restaurants_api/api/restaurants.py
@@ -1,12 +1,14 @@
 #import uuid
 #from flask import request
+
+
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
-blp = Blueprint("restaurants", __name__, description="Restaurants crud")
+blp = Blueprint("restaurants_api", __name__, description="Restaurants crud")
 
 
-@blp.route("/restaurants/<string:restaurant_id>")
+@blp.route("/restaurants_api/<string:restaurant_id>")
 class RestaurantView(MethodView):
     def get(self,restaurant_id):
         return restaurant_id
