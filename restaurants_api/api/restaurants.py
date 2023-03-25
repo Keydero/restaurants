6ecc1f6d3ff2a11f94a2aa3bae145210abe3343e
Keyproco <oussama.keddar@epitech.eu>
2023-03-26 00:20:27 +0100

    Fix service name

--- restaurants_api/api/restaurants.py
+++ restaurants_api/api/restaurants.py
@@ -1,10 +1,10 @@
 # import uuid
 # from flask import request
 
-
+from opentelemetry.sdk.resources import Resource
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, BatchSpanProcessor
 from schemas import RestaurantSchema
 
 from flask.views import MethodView
@@ -12,9 +12,13 @@ from flask_smorest import Blueprint, abort
 
 
 def configure_tracer():
+    resource = Resource.create({
+        "service.name": "restaurants",
+        "service.version": "0.0.1"
+    })
     exporter = ConsoleSpanExporter()
-    span_processor = SimpleSpanProcessor(exporter)
-    provider = TracerProvider()
+    span_processor = BatchSpanProcessor(exporter)
+    provider = TracerProvider(resource=resource)
     provider.add_span_processor(span_processor)
     trace.set_tracer_provider(provider)
 
@@ -29,7 +33,7 @@ blp = Blueprint("restaurants", __name__, description="Restaurants crud")
 class RestaurantView(MethodView):
 
     def get(self, restaurant_id):
-        with tracer.start_as_current_span("server_request",   attributes={"endpoint": f'restaurants/{restaurant_id}'}):
+        with tracer.start_as_current_span("server_request", attributes={"endpoint": f'restaurants/{restaurant_id}'}):
             with tracer.start_as_current_span("another_span", attributes={"endpoint": f'restaurants/{restaurant_id}'}):
                 return restaurant_id
 
